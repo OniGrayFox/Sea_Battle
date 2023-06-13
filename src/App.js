887dbfed61acdcf9b22163dad3e749925304d234
OniGrayFox <83906603+OniGrayFox@users.noreply.github.com>
2023-06-13 18:14:00 +0000

    Update App.js and add styles.css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <ul class="initial-ships">
+      <li>
+        <div id="fourdeck1" class="ship fourdeck"></div>
+        <div id="tripledeck1" class="ship tripledeck tripledeck1"></div>
+        <div id="tripledeck2" class="ship tripledeck tripledeck2"></div>
+      </li>
+      <li>
+        <div id="doubledeck1" class="ship doubledeck"></div>
+        <div id="doubledeck2" class="ship doubledeck doubledeck2"></div>
+        <div id="doubledeck3" class="ship doubledeck doubledeck3"></div>
+      </li>
+      <li>
+        <div id="singledeck1" class="ship singledeck"></div>
+        <div id="singledeck2" class="ship singledeck singledeck2"></div>
+        <div id="singledeck3" class="ship singledeck singledeck3"></div>
+        <div id="singledeck4" class="ship singledeck singledeck4"></div>
+      </li>
+    </ul>
   );
 }
