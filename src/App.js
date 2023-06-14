cfdaad7df1be2b9c844be19799c4646bb6903985
OniGrayFox <83906603+OniGrayFox@users.noreply.github.com>
2023-06-14 18:18:37 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,46 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div class="field field-human">
-      <div id="field_human" class="ships"></div>
+    <div>
+      <div class="field field-human">
+        <div id="field_human" class="ships"></div>
+      </div>
+      <div id="instruction" class="instruction">
+        <div id="type_placement" class="type-placement-box">
+          1.{" "}
+          <span class="link" data-target="random">
+            Случайным образом
+          </span>
+          2.{" "}
+          <span class="link" data-target="manually">
+            Методом перетаскивания.
+          </span>
+        </div>
+        <div id="ships_collection" class="ships-collection" hidden>
+          <p>
+            Перетащите мышкой корабли на игровое поле. Для установки корабля по
+            вертикали, кликните по нему правой кнопкой мышки.
+          </p>
+        </div>
+      </div>
+      <ul class="initial-ships">
+        <li>
+          <div id="fourdeck1" class="ship fourdeck"></div>
+          <div id="tripledeck1" class="ship tripledeck tripledeck1"></div>
+          <div id="tripledeck2" class="ship tripledeck tripledeck2"></div>
+        </li>
+        <li>
+          <div id="doubledeck1" class="ship doubledeck"></div>
+          <div id="doubledeck2" class="ship doubledeck doubledeck2"></div>
+          <div id="doubledeck3" class="ship doubledeck doubledeck3"></div>
+        </li>
+        <li>
+          <div id="singledeck1" class="ship singledeck"></div>
+          <div id="singledeck2" class="ship singledeck singledeck2"></div>
+          <div id="singledeck3" class="ship singledeck singledeck3"></div>
+          <div id="singledeck4" class="ship singledeck singledeck4"></div>
+        </li>
+      </ul>
     </div>
   );
 }
