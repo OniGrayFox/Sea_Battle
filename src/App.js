62f89116fb4c9b69044ae3545fce9bb0b92ed168
OniGrayFox <83906603+OniGrayFox@users.noreply.github.com>
2023-06-18 19:02:25 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,71 @@
 import "./styles.css";
 
+
+
+
+	let startGame = false;
+	// флаг установки обработчиков событий ручного размещения кораблей и
+	// редактирование их положения
+	let isHandlerPlacement = false;
+	// флаг установки обработчиков событий ведения морского боя
+	let isHandlerController = false;
+	// флаг, блокирующий действия игрока во время выстрела компьютера
+	let compShot = false;
+
+	// получаем объект элемента DOM по его ID
+	const getElement = id => document.getElementById(id);
+	// вычисляем координаты всех сторон элемента относительно окна браузера
+	// с учётом прокрутки страницы
+	const getCoordinates = el => {
+		const coords = el.getBoundingClientRect();
+		return {
+			left: coords.left + window.pageXOffset,
+			right: coords.right + window.pageXOffset,
+			top: coords.top + window.pageYOffset,
+			bottom: coords.bottom + window.pageYOffset
+		};
+	};
+
+	// игровое поле игрока
+	const humanfield = getElement('field_human');
+	// игровое поле компьютера
+	const computerfield = getElement('field_computer');
+
+	class Field {
+		// размер стороны игрового поля в px
+		static FIELD_SIDE = 330;
+		// размер палубы корабля в px
+		static SHIP_SIDE = 33;
+		// объект с данными кораблей
+		// ключём будет являться тип корабля, а значением - массив,
+		// первый элемент которого указывает кол-во кораблей данного типа,
+		// второй элемент указывает кол-во палуб у корабля данного типа
+		static SHIP_DATA = {
+			fourdeck: [1, 4],
+			tripledeck: [2, 3],
+			doubledeck: [3, 2],
+			singledeck: [4, 1]
+		};
+
+		constructor(field) {
+			// объект игрового поля, полученный в качестве аргумента
+			this.field = field;
+			// создаём пустой объект, куда будем заносить данные по каждому созданному кораблю
+			// эскадры, подробно эти данные рассмотрим при создании объектов кораблей
+			this.squadron = {};
+			// двумерный массив, в который заносятся координаты кораблей, а в ходе морского
+			// боя, координаты попаданий, промахов и заведомо пустых клеток
+			this.matrix = [];
+			// получаем координаты всех четырёх сторон рамки игрового поля относительно начала
+			// document, с учётом возможной прокрутки по вертикали 
+			let { left, right, top, bottom } = getCoordinates(this.field);
+			this.fieldLeft = left;
+			this.fieldRight = right;
+			this.fieldTop = top;
+			this.fieldBottom = bottom;
+		}
+
+		static createMatrix
 export default function App() {
   return (
     <div>
